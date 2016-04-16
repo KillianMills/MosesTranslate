59ad9d656d7c5f4cb71d44c5af045a83492d918f
Killian Mills <killian.mills@gmail.com>
2016-04-16 17:34:18 +0100

    alert box source

--- WebSMT/javascript/translate.js
+++ WebSMT/javascript/translate.js
@@ -1,3 +1,14 @@
+var demoTxtChanged = false;
+
+// Ajax stuff
+
+var xhr = null;
+var startTime = 0;
+
+function translateText(){
+	translate();
+}
+
 function translate()
 {
 	// get source, result and translation menu
@@ -8,6 +19,8 @@ function translate()
 
     document.getElementById("result").value = "";
     sorceText = sourceText.trim();
+	
+	 alert(sourceText);
 
     if (sorceText.length == 0){
         return;
@@ -34,4 +47,55 @@ function translate()
     startTime = Date.now();
 
     xhr.send(params);
+}
+
+function handleNewText()
+{
+    if (xhr == null)
+        return; // strange
+
+    if (xhr.responseText != null && xhr.responseText.trim() == "ERROR") {
+      alert('An error occurred. Sorry.');
+      document.getElementById("subTrans").disabled = false;
+      document.getElementById("wait").style.display = "none";
+      return;
+    }
+
+    moreText = (xhr.readyState == 3 || xhr.readyState == 4) &&
+              !(navigator.userAgent && navigator.userAgent.indexOf("MSIE") >= 0);
+
+    if (moreText)
+    {
+        document.getElementById("resultText").value = xhr.responseText;
+    }
+
+    if (xhr.readyState == 4)
+    {
+        document.getElementById("resultText").value = xhr.responseText;
+        document.getElementById("translateButton").disabled = false;
+        //document.getElementById("wait").style.display = "none";
+        trTime = Date.now() - startTime;
+        if (startTime != 0 && trTime > 0) {
+           var displayTime = trTime / 1000;
+           displayTime = displayTime.toFixed(1);
+           document.getElementById("trTime").innerHTML = displayTime + " s";
+           startTime = 0;
+        }
+    } 
+}
+
+function newHttp()
+{
+    if (window.XMLHttpRequest)
+    {
+        return new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject)
+    {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else
+    {
+        return null;
+    }
 }
